4d46c7d71fd97c936639241f5fc580ea70284217
Weverson <weverson_santos@live.com>
2018-04-15 10:26:29 -0300

    Pegando os títulos das vagas

--- InfojobCrawler/src/control/Crawler.java
+++ InfojobCrawler/src/control/Crawler.java
@@ -18,17 +18,38 @@ public class Crawler {
 	public void getData() throws IOException {
 		String website = "https://www.sine.com.br/vagas-empregos-em-feira-de-santana-ba/";
 		String function = "eletricista";
-		//String website = "https://community.oracle.com/";
 		URL sourceUrl = new URL(website + function);
 		System.out.println("Iniciando download...");
 		BufferedReader in = new BufferedReader(new InputStreamReader(sourceUrl.openStream()));
-		System.out.println("passou");
+		//System.out.println("passou");
 		String line;
+		Job job;
+		StringBuilder jobTitle = new StringBuilder();
+		boolean buildingJobTitle = false;
 		while ((line=in.readLine()) != null) {
-			if ((line.contains("<h1"))) {
-				System.out.println(line.substring(line.indexOf(">") + 1, line.indexOf("</h1>")));
+			if ((line.contains("item-block col-md-12"))) {
+				job = new Job();
+				jobTitle = new StringBuilder();
+			} else if (line.contains("titulo_vaga")) {
+				buildingJobTitle = true;
+			} else if (buildingJobTitle) {
+				if (line.contains("<span>")) {
+					buildingJobTitle = false;
+					System.out.println(jobTitle.toString());
+				}
+				if (line.trim().length() > 1) {
+					if (line.contains("fa-wheelchair")) {
+						line = line.substring(line.indexOf("</i>") + 4);
+						if (line.charAt(0) == 32) {
+							line = line.substring(1);
+						}
+					}
+					line = line.substring(line.indexOf(line.trim()));
+					jobTitle.append(line);
+				}
 			}
 		}
 		in.close(); 
 	}
+	//System.out.println(line.substring(line.indexOf(">") + 1, line.indexOf("</h1>")));
 }
---
+++ InfojobCrawler/src/control/Job.java
@@ -0,0 +1,9 @@
+package control;
+
+public class Job {
+	String jobTitle;
+	String jobID;
+	String city;
+	String state;
+	int salary;
+}
