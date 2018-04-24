935f87c199848403f38d80fa25a4bff6ed04dd34
Weverson Gomes <weverson_santos@live.com>
2018-04-24 17:52:29 -0300

    Pegando json do sine

--- InfojobCrawler/src/control/Crawler.java
+++ InfojobCrawler/src/control/Crawler.java
@@ -3,22 +3,28 @@ package control;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Hashtable;
 import java.util.LinkedList;
+
+import javax.json.JsonObject;
+
+import com.mysql.cj.xdevapi.JsonParser;
 
 
 public class Crawler {
 	public static void main(String[] args) {
 		Crawler crawler = new Crawler();
 		try {
-			String url = crawler.getAddressFor("", "");
-			crawler.getData(url);
+			//String url = crawler.getAddressFor("", "");
+			//crawler.getData(url);
+			String jsonContent = crawler.getContent("https://www.sine.com.br/api/v1.0/Job/List?idFuncao=8741&idCidade=0&pagina=2&pesquisa=&ordenacao=1&idUsuario=NaN");
+			crawler.getAndParseJson(jsonContent);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+	/*
 	public String getAddressFor(String city, String function) {
 		//System.setProperty("https.proxyHost", "10.65.16.2"); 
 		//System.setProperty("http.proxyPort", "3128");
@@ -33,6 +39,26 @@ public class Crawler {
 			address.append("/eletricista");
 		}
 		return address.toString();
+	}*/
+	
+	public String getContent(String url) throws IOException {
+		URL sourceUrl = new URL(url);
+		System.out.println("Iniciando download do json...");
+		BufferedReader in = new BufferedReader(new InputStreamReader(sourceUrl.openStream()));
+		String line;
+		StringBuilder jsonContent = new StringBuilder();
+		while ((line=in.readLine()) != null) {
+			jsonContent.append(line);
+		}
+		return jsonContent.toString();
+	}
+	
+	public void getAndParseJson(String jsonContent) {
+		/*JsonParser parser = new JsonParser();
+		JsonObject jsonObj;
+		jsonObj = (JsonObject) parser.parse(content);
+		String name = jsonObj.getString("name");
+		int level = jsonObj.getInt("level");*/
 	}
 	
 	public void getData(String url) throws IOException {
